f9ac23c5202d15459d2ecc90424f8c19ab088e59
MOHIT-JANGIR <mohitjangir700@gmail.com>
2021-07-05 12:30:58 +0530

    first commit

---
+++ rto3.py
@@ -0,0 +1,12 @@
+import json,requests,xmltodict
+
+def get_vehicle_info(plate_number):
+    r = requests.get("http://www.regcheck.org.uk/api/reg.asmx/CheckIndia?RegistrationNumber={0}&username=mohit_jangir".format(str(plate_number)))
+    data = xmltodict.parse(r.content)
+    jdata = json.dumps(data)
+    data_final = json.loads(jdata)
+    data_print = json.loads(data_final['Vehicle']['vehicleJson'])
+    print(data_print)
+    return data_print
+
+get_vehicle_info("MH20EE7598")

c61cb2c1b2561271631c036876dcf1c9642a6cc2
MOHIT-JANGIR <mohitjangir700@gmail.com>
2021-07-05 12:26:03 +0530

    first commit

---
+++ textract.py
@@ -0,0 +1,22 @@
+import boto3
+
+# Document
+documentName = "number-plate-detection.png"
+
+# Read document content
+with open(documentName, 'rb') as document:
+    imageBytes = bytearray(document.read())
+
+# Amazon Textract client
+textract = boto3.client('textract')
+
+# Call Amazon Textract
+response = textract.detect_document_text(Document={'Bytes': imageBytes})
+
+#print(response)
+
+# Print detected text
+for item in response["Blocks"]:
+    if item["BlockType"] == "LINE" and item["Confidence"]>=95:
+        #print ('\033[94m' +  item["Text"] + '\033[0m')
+         print(item["Text"])
